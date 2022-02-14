ab1b246fec0efc2c5d54d4767d9fa69594175775
Michael Desanker <mdesanker@gmail.com>
2022-02-13 21:40:57 -0500

    Add some middleware packages to app.ts

--- app.ts
+++ app.ts
@@ -2,10 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/mongoConfig";
 dotenv.config();
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
 
 const app = express();
 
 connectDB();
+
+app.use(
+  cors({
+    origin: [/\localhost/, /\mdesanker.github.io/],
+    credentials: true,
+  })
+);
+app.use(helmet());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = (process.env.PORT as string) || 8000;
 
