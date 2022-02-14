a11366d8253d36404b71858bc99c4374a2af6883
Michael Desanker <mdesanker@gmail.com>
2022-02-14 06:05:44 -0500

    Add test for register user route

--- routes/__tests__/auth.test.ts
+++ routes/__tests__/auth.test.ts
@@ -16,6 +16,65 @@ afterAll(() => {
   mongoose.connection.close();
 });
 
+//POST ROUTES
+describe("POST /auth/register", () => {
+  it("return new user", async () => {
+    const res = await request(app).post("/auth/register").send({
+      userName: "test",
+      email: "test@gmail.com",
+      password: "password",
+      userType: "user",
+    });
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("email");
+    expect(res.body.email).toEqual("test@gmail.com");
+  });
+
+  it("return error for missing login details", async () => {
+    const res = await request(app).post("/auth/register").send({
+      userName: "noEmail",
+      email: "",
+      password: "password",
+      userType: "user",
+    });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors[0].msg).toEqual("Email is required");
+  });
+
+  it("return error for password too short", async () => {
+    const res = await request(app).post("/auth/register").send({
+      username: "user",
+      email: "user@gmail.com",
+      password: "pass",
+      userType: "user",
+    });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors[0].msg).toEqual(
+      "Password must be at least 6 characters long"
+    );
+  });
+
+  it("return error if email already associated with account", async () => {
+    const res = await request(app).post("/auth/register").send({
+      userName: "test2",
+      email: "test@gmail.com",
+      password: "password",
+      userType: "user",
+    });
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors[0].msg).toEqual(
+      "An account is already associated with that email address"
+    );
+  });
+});
+
 // GET ROUTES
 describe("GET /auth", () => {
   it("return 200 status code", async () => {
@@ -24,3 +83,5 @@ describe("GET /auth", () => {
     expect(res.statusCode).toEqual(200);
   });
 });
+
+//
