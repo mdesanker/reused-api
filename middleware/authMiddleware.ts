46ea5298439a960c848f0ac20013a45a390582ad
Michael Desanker <mdesanker@gmail.com>
2022-02-14 10:47:41 -0500

    Add middleware auth function

---
+++ middleware/authMiddleware.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+interface IUserRequest extends Request {
+  user: any;
+}
+
+const authMiddleware = async (
+  req: IUserRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  if (!token) {
+    return res.status(401).json({ msg: "Authorization denied: invalid token" });
+  }
+
+  // Verify token
+  try {
+    const decoded: any = jwt.verify(token, process.env.JWT_KEY as string);
+
+    req.user = decoded.user;
+    next();
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(401).json({ msg: "Invalid token" });
+    }
+  }
+};
+
+export = authMiddleware;
