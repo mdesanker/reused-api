e133fd12f0c43c998c2b5e0c024828d4fad0ebd1
Michael Desanker <mdesanker@gmail.com>
2022-02-14 05:36:35 -0500

    Create mongo test config file

---
+++ config/mongoTestConfig.ts
@@ -0,0 +1,19 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+const initializeServer = async () => {
+  try {
+    const mongoServer = await MongoMemoryServer.create();
+    const mongoUri = mongoServer.getUri();
+
+    await mongoose.connect(mongoUri);
+
+    console.log(`MongoDB connected to ${mongoUri}`);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error(err.message);
+    }
+  }
+};
+
+export default initializeServer;
