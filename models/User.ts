ffa46882729d6bafcb1a95f539206e2484fbd104
Michael Desanker <mdesanker@gmail.com>
2022-02-13 21:35:54 -0500

    Add user model

---
+++ models/User.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+export interface IUser {
+  username: string;
+  email: string;
+  password: string;
+  userType: "user" | "admin";
+}
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, minlength: 6 },
+  userType: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+    required: true,
+  },
+});
+
+export default model<IUser>("User", UserSchema);
