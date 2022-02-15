3b390217603928753060ee62d60c7ae28a8d6ee9
Michael Desanker <mdesanker@gmail.com>
2022-02-15 15:58:42 -0500

    Add test for get all products route

--- routes/__tests__/app.ts
+++ routes/__tests__/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 import authRouter from "../../routes/api/auth";
 import userRouter from "../../routes/api/user";
 import categoryRouter from "../../routes/api/category";
+import productRouter from "../../routes/api/product";
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
 app.use("/category", categoryRouter);
+app.use("/product", productRouter);
 
 export = app;
---
+++ routes/__tests__/product.test.ts
@@ -0,0 +1,46 @@
+import app from "./app";
+import initializeTestServer from "../../config/mongoTestConfig";
+import request from "supertest";
+import mongoose from "mongoose";
+import seedDB from "./seed";
+
+// GLOBAL VARIABLES
+let janeToken: string;
+let johnToken: string;
+
+// TEST SETUP
+beforeAll(async () => {
+  await initializeTestServer();
+  await seedDB();
+
+  // Generate jane token
+  const janeLogin = await request(app).post("/auth/login").send({
+    email: "jane@gmail.com",
+    password: "password",
+  });
+
+  janeToken = janeLogin.body.token;
+
+  // Generate john token
+  const johnLogin = await request(app).post("/auth/login").send({
+    email: "john@gmail.com",
+    password: "password",
+  });
+
+  johnToken = johnLogin.body.token;
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
+
+// GET ROUTES
+describe("GET /product/all", () => {
+  it("return array of all products", async () => {
+    const res = await request(app).get("/product/all");
+
+    expect(res.statusCode).toEqual(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+});
