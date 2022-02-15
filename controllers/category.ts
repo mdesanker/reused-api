0cf84e64c88ff599c8dfd6f828c8bc2de2861f4f
Michael Desanker <mdesanker@gmail.com>
2022-02-15 06:48:07 -0500

    Add route to get all categories

--- controllers/category.ts
+++ controllers/category.ts
@@ -1,7 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import Category from "../models/Category";
 
 const all = async (req: Request, res: Response, next: NextFunction) => {
-  res.send("Category router test");
+  try {
+    const categories = await Category.find({});
+
+    res.json(categories);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).send("Server error");
+    }
+  }
 };
 
 export default { all };
