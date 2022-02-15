f20466db6b007a41b4e2b4cb47fcd692026f566e
Michael Desanker <mdesanker@gmail.com>
2022-02-15 07:00:08 -0500

    Add tests for get category by id route

--- routes/__tests__/category.test.ts
+++ routes/__tests__/category.test.ts
@@ -6,6 +6,9 @@ import seedDB from "./seed";
 
 // GLOBAL VARIABLES
 let janeToken: string;
+const electronicsId: string = "620b90e0c2b6e006dde0cb41";
+const apparelId: string = "620b90e0c2b6e006dde0cb42";
+let invalidId: string = "620b90e0c2b6e006dde00000";
 
 // TEST SETUP
 beforeAll(async () => {
@@ -33,5 +36,23 @@ describe("GET /category/all", () => {
     expect(res.statusCode).toEqual(200);
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body.length).toBeGreaterThan(0);
+  });
+});
+
+describe("GET /category/:id", () => {
+  it("return category by id", async () => {
+    const res = await request(app).get(`/category/${electronicsId}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body._id).toEqual(electronicsId);
+    expect(res.body).toHaveProperty("name");
+  });
+
+  it("return error for invalid category id", async () => {
+    const res = await request(app).get(`/category/${invalidId}`);
+
+    expect(res.statusCode).toEqual(404);
+    expect(res.body).toHaveProperty("errors");
+    expect(res.body.errors[0].msg).toEqual("Invalid category id");
   });
 });
