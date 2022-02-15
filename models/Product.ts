b72e1ac4db8ae761d38f66979791979c7605fa54
Michael Desanker <mdesanker@gmail.com>
2022-02-15 12:47:07 -0500

    Create product model

---
+++ models/Product.ts
@@ -0,0 +1,31 @@
+import { Schema, Types, model } from "mongoose";
+
+export interface IProduct {
+  _id?: String;
+  name: String;
+  price: Number;
+  description?: String;
+  condition: "new" | "like new" | "excellent" | "good" | "fair" | "poor";
+  category: Types.ObjectId;
+  date: Date;
+  images: String[];
+  owner: Types.ObjectId;
+}
+
+const ProductSchema = new Schema<IProduct>({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String },
+  condition: {
+    type: String,
+    enum: ["new", "like new", "excellent", "good", "fair", "poor"],
+    required: true,
+    default: "good",
+  },
+  category: { type: Schema.Types.ObjectId, ref: "Category", required: true },
+  date: { type: Date, default: Date.now },
+  images: [{ type: String }],
+  owner: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+export default model<IProduct>("Product", ProductSchema);
