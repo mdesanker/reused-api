fea4c839f7507c6373e0a61f1e92e740bc7c3b51
Michael Desanker <mdesanker@gmail.com>
2022-02-15 21:39:56 -0500

    Add route to get products for category

--- controllers/product.ts
+++ controllers/product.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { check, validationResult } from "express-validator";
+import Category from "../models/Category";
 import Product from "../models/Product";
 
 const all = async (req: Request, res: Response, next: NextFunction) => {
@@ -33,4 +34,28 @@ const product = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default { all, product };
+const category = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  try {
+    // Check id is valid
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(404).json({ errors: [{ msg: "Invalid category id" }] });
+    }
+
+    // Get category products
+    const products = await Product.find({ category: id }).populate(
+      "category owner"
+    );
+
+    res.json(products);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).send("Server error");
+    }
+  }
+};
+
+export default { all, product, category };
--- routes/api/product.ts
+++ routes/api/product.ts
@@ -4,5 +4,6 @@ import productController from "../../controllers/product";
 
 product.get("/all", productController.all);
 product.get("/:id", productController.product);
+product.get("/category/:id", productController.category);
 
 export = product;
