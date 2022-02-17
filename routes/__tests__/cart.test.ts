2ab2cbc01c4aafe373c3d596cfd4cae3d44d0063
Michael Desanker <mdesanker@gmail.com>
2022-02-17 05:09:57 -0500

    Add tests for get user cart route

--- routes/__tests__/cart.test.ts
+++ routes/__tests__/cart.test.ts
@@ -7,8 +7,12 @@ import seedDB from "./seed";
 // GLOBAL VARIABLES
 let janeToken: string;
 let johnToken: string;
+const janeId: string = "620ab20b2dffe3ba60353a22";
+const johnId: string = "620ab20b2dffe3ba60353a23";
+const userId: string = "620ab20b2dffe3ba60353a99";
+const invalidUserId: string = "620ab20b2dffe3ba60300000";
 const cartId: string = "620e1a4b2dc4a3341164625a";
-const johnCardId: string = "620e1a4b2dc4a3341164625c";
+const johnCartId: string = "620e1a4b2dc4a3341164625c";
 const electronicsId: string = "620b90e0c2b6e006dde0cb41";
 const apparelId: string = "620b90e0c2b6e006dde0cb42";
 let invalidId: string = "620b90e0c2b6e006dde00000";
@@ -47,5 +51,22 @@ describe("GET /cart/all", () => {
     expect(res.statusCode).toEqual(200);
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body.length).toBeGreaterThan(0);
+  });
+});
+
+describe("GET /cart/user/:id", () => {
+  it("return cart for user id", async () => {
+    const res = await request(app).get(`/cart/user/${johnId}`);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.user._id).toEqual(johnId);
+    expect(res.body).toHaveProperty("products");
+  });
+
+  it("return error for invalid user id", async () => {
+    const res = await request(app).get(`/cart/user/${invalidId}`);
+
+    expect(res.statusCode).toEqual(404);
+    expect(res.body.errors[0].msg).toEqual("Invalid user id");
   });
 });
