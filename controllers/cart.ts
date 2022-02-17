e79022b45c5fcbc9a4a49d2efdf49dfdb3235ec5
Michael Desanker <mdesanker@gmail.com>
2022-02-17 04:56:43 -0500

    Get all carts route

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -1,7 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import Cart from "../models/Cart";
 
 const all = async (req: Request, res: Response, next: NextFunction) => {
-  res.send("GET cart test");
+  try {
+    const carts = await Cart.find({}).populate("user");
+
+    res.json(carts);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).send("Server error");
+    }
+  }
 };
 
 export default { all };
