e819599742ed08c40eb78c53bb122620d745e9be
Michael Desanker <mdesanker@gmail.com>
2022-02-17 05:30:40 -0500

    Add get user cart route

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import Cart from "../models/Cart";
+import User from "../models/User";
 
 const all = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -13,4 +14,26 @@ const all = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export default { all };
+const user = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+
+  try {
+    // Check id is valid
+    const user = await User.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ errors: [{ msg: "Invalid user id" }] });
+    }
+
+    // Return user cart
+    const cart = await Cart.findOne({ user: user.id }).populate("user");
+
+    res.json(cart);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(500).send("Server error");
+    }
+  }
+};
+
+export default { all, user };
--- routes/api/cart.ts
+++ routes/api/cart.ts
@@ -3,5 +3,5 @@ const cart = Router();
 import cartController from "../../controllers/cart";
 
 cart.get("/all", cartController.all);
-
+cart.get("/user/:id", cartController.user);
 export = cart;
